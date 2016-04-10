cb2f95d97633bc152111d2ff0517588a7103ea35
Pepijn Kokke <pepijn.kokke@gmail.com>
2016-04-10 16:18:15 +0200

    Wrote new method for initialisation (uniform/random).

--- src/ibm2.py
+++ src/ibm2.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
-from itertools   import chain
+from itertools   import chain,product
 from msgpack     import pack,unpack
 from random      import random
 from sys         import stdout
+
+import numpy as np
 
 class IBM2:
 
@@ -14,10 +16,6 @@ class IBM2:
     def dump(self,stream):
         pack((self.t,self.q), stream)
 
-    @classmethod
-    def random(cls):
-        return cls(defaultdict(random), defaultdict(random))
-
     def __init__(self, t, q):
         self.t = t
         self.q = q
@@ -25,17 +23,16 @@ class IBM2:
     def em_train(self,corpus,n=10):
         for k in range(1,n+1):
             self.em_iter(corpus,passnum=k)
-            stdout.write("\rPass %2d: 100.000%%\n" % k)
-            stdout.flush()
+            print("\rPass %2d: 100.00%%" % k)
 
-    def em_iter(self,corpus,passnum=0):
+    def em_iter(self,corpus,passnum=1):
 
         c1 = defaultdict(float) # ei aligned with fj
         c2 = defaultdict(float) # ei aligned with anything
         c3 = defaultdict(float) # wj aligned with wi
         c4 = defaultdict(float) # wi aligned with anything
 
-        for k, (e, f) in enumerate(corpus):
+        for k, (f, e) in enumerate(corpus):
 
             if k % 100 == 0:
                 stdout.write("\rPass %2d: %6.2f%%" % (passnum, (100*k) / float(len(corpus))))
@@ -63,26 +60,74 @@ class IBM2:
         self.t = defaultdict(float,{k: v / c2[k[1:]] for k,v in c1.iteritems() if v > 0.0})
         self.q = defaultdict(float,{k: v / c4[k[1:]] for k,v in c3.iteritems() if v > 0.0})
 
+    @classmethod
+    def random(cls,corpus):
+        return cls.with_generator(
+            corpus, lambda n: np.random.dirichlet(np.ones(n),size=1)[0])
+
+    @classmethod
+    def uniform(cls,corpus):
+        return cls.with_generator(corpus, lambda n: 1 / float(n))
+
+    @classmethod
+    def with_generator(cls,corpus,g):
+
+        # "Compute all possible alignments..."
+        lens   = set()
+        aligns = defaultdict(set)
+
+        for k, (f, e) in enumerate(corpus):
+            stdout.write("\rInit     %5.2f%%" % ((33*k) / float(len(corpus))))
+            stdout.flush()
+
+            lens.add((len(e), len(f)))
+
+            for (f, e) in product(f, [None] + e):
+                aligns[e].add((f, e))
+
+        # "Compute initial probabilities for each alignment..."
+        k = 0
+        t = dict()
+        for e, aligns_to_e in aligns.iteritems():
+            stdout.write("\rInit     %5.2f%%" % (33 + ((33*k) / float(len(aligns)))))
+            stdout.flush()
+            k += 1
+
+            p_values = g(len(aligns_to_e))
+            t.update(zip(aligns_to_e,p_values))
+
+        # "Compute initial probabilities for each distortion..."
+        q = dict()
+        for k, (l, m) in enumerate(lens):
+            stdout.write("\rInit     %5.2f%%" % (66 + ((33*k) / float(len(lens)))))
+            stdout.flush()
+
+            for i in range(1,m):
+                p_values = g(l)
+                for j in range(0,l):
+                    q[(j,i,l,m)] = p_values[j]
+        print "\rInit     100.00%"
+
+        return cls(t,q)
+
 
 def read_corpus(path):
     """Read a file as a list of lists of words."""
     with open(path,'r') as f:
-        return[ ln.strip().split() for ln in f ]
+        return [ ln.strip().split() for ln in f ]
 
 
 if __name__ == "__main__":
 
     fr_corpus_path = '../data/training/hansards.36.2.f'
     en_corpus_path = '../data/training/hansards.36.2.e'
-    model_path_1   = '../data/training/hansards.36.2.pass1.pack'
-    model_path_5   = '../data/training/hansards.36.2.pass5.pack'
-    model_path_10  = '../data/training/hansards.36.2.pass10.pack'
+    model_path_0   = '../data/hansards.36.2.rand.pack'
+    model_path_1   = '../data/hansards.36.2.rand.pass1.pack'
+    model_path_5   = '../data/hansards.36.2.rand.pass5.pack'
+    model_path_10  = '../data/hansards.36.2.rand.pass10.pack'
+    model_path_20  = '../data/hansards.36.2.rand.pass20.pack'
     corpus         = zip(read_corpus(fr_corpus_path),read_corpus(en_corpus_path))
+    corpus         = corpus[:1000]
 
-    with open(model_path_1,'r') as stream:
-        ibm = IBM2.load(stream)
-
-    ibm.em_train(corpus,n=4)
-
-    with open(model_path_5,'w') as stream:
-        ibm.dump(stream)
+    ibm = IBM2.random(corpus)
+    ibm.em_train(corpus,n=2)
