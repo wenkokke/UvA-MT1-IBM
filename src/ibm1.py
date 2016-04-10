62cc4a12af18e4caf644adb08c2080786d00dcc1
Wen Kokke <wenkokke@users.noreply.github.com>
2016-04-10 13:35:10 +0200

    changed staticmethod to classmethod in ibm1; implemented ibm2

--- src/ibm1.py
+++ src/ibm1.py
@@ -4,23 +4,22 @@ from msgpack     import pack,unpack
 from random      import random
 from sys         import stdout
 
-class IBM2:
+class IBM1:
 
     @classmethod
     def load(cls,stream):
-        (t,q) = unpack(stream,use_list=False)
-        return cls(defaultdict(float,t), defaultdict(float,q))
+        t = unpack(stream,use_list=False)
+        return cls(defaultdict(float,t))
 
     def dump(self,stream):
-        pack((self.t,self.q), stream)
+        pack(self.t, stream)
 
     @classmethod
     def random(cls):
-        return cls(defaultdict(random), defaultdict(random))
+        return cls(defaultdict(random))
 
-    def __init__(self, t, q):
+    def __init__(self, t):
         self.t = t
-        self.q = q
 
     def em_train(self,corpus,n=10):
         for k in range(1,n+1):
@@ -28,12 +27,10 @@ class IBM2:
             stdout.write("\rPass %2d: 100.000%%\n" % k)
             stdout.flush()
 
-    def em_iter(self,corpus,passnum=0):
+    def em_iter(self,corpus,passnum=1):
 
         c1 = defaultdict(float) # ei aligned with fj
         c2 = defaultdict(float) # ei aligned with anything
-        c3 = defaultdict(float) # wj aligned with wi
-        c4 = defaultdict(float) # wi aligned with anything
 
         for k, (e, f) in enumerate(corpus):
 
@@ -43,11 +40,11 @@ class IBM2:
             l = len(e)
             m = len(f)
             e = [None] + e
+            q = 1 / float(len(e))
 
             for i in range(1,m):
 
-                num = [ self.q[(j,i,l,m)] * self.t[(f[i - 1], e[j])]
-                        for j in range(0,l) ]
+                num = [ q * self.t[(f[i - 1], e[j])] for j in range(0,l) ]
                 den = float(sum(num))
 
                 for j in range(0,l):
@@ -56,11 +53,8 @@ class IBM2:
 
                     c1[(f[i - 1], e[j])] += delta
                     c2[(e[j],)]          += delta
-                    c3[(j,i,l,m)]        += delta
-                    c4[(i,l,m)]          += delta
 
         self.t = defaultdict(float,{k: v / c2[k[1:]] for k,v in c1.iteritems() if v > 0.0})
-        self.q = defaultdict(float,{k: v / c4[k[1:]] for k,v in c3.iteritems() if v > 0.0})
 
 
 def read_corpus(path):
@@ -70,16 +64,17 @@ def read_corpus(path):
 
 fr_corpus_path = '../data/training/hansards.36.2.f'
 en_corpus_path = '../data/training/hansards.36.2.e'
-model_path_1   = '../data/training/hansards.36.2.pass1.pack'
-model_path_5   = '../data/training/hansards.36.2.pass5.pack'
-model_path_10  = '../data/training/hansards.36.2.pass10.pack'
+model_path_1   = '../data/training/hansards.36.2.ibm1.pass1.pack'
+model_path_5   = '../data/training/hansards.36.2.ibm1.pass5.pack'
+model_path_10  = '../data/training/hansards.36.2.ibm1.pass10.pack'
 corpus         = zip(read_corpus(fr_corpus_path),read_corpus(en_corpus_path))
 
 
-with open(model_path_1,'r') as stream:
-    ibm = IBM2.load(stream)
+#with open(model_path_1,'r') as stream:
+#    ibm = IBM1.load(stream)
 
-ibm.em_train(corpus,n=4)
+ibm = IBM1.random()
+ibm.em_train(corpus,n=1)
 
-with open(model_path_5,'w') as stream:
+with open(model_path_1,'w') as stream:
     ibm.dump(stream)
