e2aafc8e30d8218e0318de693ac58d7dec3fdd1b
Thijs Scheepers <thijs@label305.com>
2016-04-12 10:30:58 +0200

    Added prediction for model 1

--- src/ibm2.py
+++ src/ibm2.py
@@ -4,6 +4,7 @@ from msgpack     import pack,unpack
 from random      import random
 from sys         import stdout
 from os          import path
+import operator
 
 import numpy as np
 
@@ -60,6 +61,19 @@ class IBM2:
 
         self.t = defaultdict(float,{k: v / c2[k[1:]] for k,v in c1.iteritems() if v > 0.0})
         self.q = defaultdict(float,{k: v / c4[k[1:]] for k,v in c3.iteritems() if v > 0.0})
+
+    def predict_alignment_model1(self,english,french):
+        
+        r = []
+        for f in french:
+            es = {k: v for k, v in self.t.iteritems() if k[0] == f and k[1] in english}
+            if len(es) == 0:
+                r.append(0)
+            else:
+                e = max(es.iteritems(), key=operator.itemgetter(1))[0][1]
+                r.append(english.index(e) + 1)
+
+        return r
 
     @classmethod
     def random(cls):
@@ -121,18 +135,29 @@ if __name__ == "__main__":
     corpus_path = '../data/training/hansards.36.2'
     fr_corpus_path = corpus_path + '.f'
     en_corpus_path = corpus_path + '.e'
-    corpus = zip(read_corpus(fr_corpus_path), read_corpus(en_corpus_path))
 
-    ibm = IBM2.uniform(corpus)
+    pack_path = corpus_path + '.20.uniform2.pack'
+    with open(pack_path, 'r') as stream:
+        ibm = IBM2.load(stream)
+        result = ibm.predict_alignment_model1('the dog barks'.split(),'le chien aboie'.split())
+        print result
 
-    for s in range(1, 21):
-        pack_path = corpus_path + '.' + str(s) + '.uniform2.pack'
-        if path.isfile(pack_path):
-            with open(pack_path, 'r') as stream:
-                ibm = IBM2.load(stream)
-            print "Loaded %s" % (pack_path)
-        else:
-            ibm.em_train(corpus, n=1, s=s)
-            with open(pack_path, 'w') as stream:
-                ibm.dump(stream)
-            print "Dumped %s" % (pack_path)
+
+    # corpus_path = '../data/training/hansards.36.2'
+    # fr_corpus_path = corpus_path + '.f'
+    # en_corpus_path = corpus_path + '.e'
+    # corpus = zip(read_corpus(fr_corpus_path), read_corpus(en_corpus_path))
+    #
+    # ibm = IBM2.uniform(corpus)
+    #
+    # for s in range(1, 21):
+    #     pack_path = corpus_path + '.' + str(s) + '.uniform2.pack'
+    #     if path.isfile(pack_path):
+    #         with open(pack_path, 'r') as stream:
+    #             ibm = IBM2.load(stream)
+    #         print "Loaded %s" % (pack_path)
+    #     else:
+    #         ibm.em_train(corpus, n=1, s=s)
+    #         with open(pack_path, 'w') as stream:
+    #             ibm.dump(stream)
+    #         print "Dumped %s" % (pack_path)
